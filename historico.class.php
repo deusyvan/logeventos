8d297ce49b274e12cfa8345aa2dd527769502b5e
2º Sgt QE Deusyvan <deusyvan.silva@eb.mil.br>
2020-02-05 10:09:26 -0300

    Criando e inserindo uma ação de abrir a pagina inicial

---
+++ historico.class.php
@@ -0,0 +1,21 @@
+<?php 
+class Historico {
+    
+    private $pdo;
+    public function __construct()
+    {
+        $this->pdo = new PDO("mysql:dbname=logeventos;host=localhost", "dfsweb","28033011");
+    }
+    
+    public function registrar($acao)
+    {
+        $ip = $_SERVER['REMOTE_ADDR'];
+        
+        $sql = "INSERT INTO historico SET ip = :ip, data_acao = NOW(), acao = :acao";
+        $sql = $this->pdo->prepare($sql);
+        $sql->bindValue(":acao", $acao);
+        $sql->bindValue(":ip", $ip);
+        $sql->execute();
+        var_dump($sql);
+    }
+}
